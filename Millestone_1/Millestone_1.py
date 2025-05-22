a51a10b7b9684f80c222813b21cbcca3d569708d
eliascueter <cueterdiazje@wsc.school.nz>
2025-05-22 12:13:32 +1200

    Updated Millestone 1

---
+++ Millestone_1/Millestone_1.py
@@ -0,0 +1,7 @@
+#Rubiks cube volume calculator
+print("Hi, lets calculate the volume of your rubiks cube")
+Lenght = int(input("How long is the the cube?"))
+Height = int(input("how tall is the cube?"))
+Width = int(input("How width is the cube?"))
+print ("The Height is {} The Width is {} The Lenght is {}".format (Height, Width, Lenght))
+print("The Volume of the Rubik Cube is {}".format(Height * Width * Lenght))
